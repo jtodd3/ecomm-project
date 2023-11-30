570f89d018eb5c964da4ed7153feb785222b5104
Josh Todd <millennial.todd@gmail.com>
2023-11-30 16:44:11 -0600

    chore: add navigate directory-iten

--- src/components/directory-item/directory-item.jsx
+++ src/components/directory-item/directory-item.jsx
@@ -1,17 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import {
   DirectoryItemContainer,
   BackgroundImage,
   Body,
 } from "./directory-item.styles.jsx";
 
-export default function DirectoryItem({ category: { imageUrl, title } }) {
+export default function DirectoryItem({ category }) {
+  const { imageUrl, title } = category;
+  const navigate = useNavigate();
+
+  const handleOnNavigate = () => navigate(`shop/${title}`);
+
   return (
-    <DirectoryItemContainer>
-      <BackgroundImage
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
+    <DirectoryItemContainer onClick={handleOnNavigate}>
+      <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
